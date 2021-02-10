8cc48291bd70a658b7fbe62ba0bc5759be386112
Timothy Savard <tpsavard@gmail.com>
2021-02-09 21:49:34 -0800

    Applied a revised design.

--- assets/scripts/quote.js
+++ assets/scripts/quote.js
@@ -2,7 +2,7 @@
 // Quotes should not exceed 50 characters.
 
 quotes = [
-    'Computers aren’t the thing; they’re the thing that gets us to the thing.',
+    'Computers aren’t the thing;<br>they’re the thing that gets us to the thing.',
     'So much for the little training cruise.',
     'The only good system is a sound system.',
     'Festina Lente',
